445a2d5c89f3df9c685c4cead108766b4c3526b1
TimonKeller <55238597+TimonKeller@users.noreply.github.com>
2022-05-04 14:07:21 +0200

    updates

---
+++ IP6_BasicShowroom 2/Assets/CameraPosition.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+            Debug.Log("Timon's Camera" + Camera.main.transform.position);
+            Debug.Log("Camera Vector" + Camera.main.transform.forward);
+
+        
+    }
+}
