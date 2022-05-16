2f364c93cb6030f6d96693e9d40902e89530785d
TimonKeller <55238597+TimonKeller@users.noreply.github.com>
2022-05-16 12:20:40 +0200

    updates

--- IP6_BasicShowroom 2/Assets/Mini First Person Controller/Scripts/FirstPersonLook.cs
+++ IP6_BasicShowroom 2/Assets/Mini First Person Controller/Scripts/FirstPersonLook.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Database;
+using UnityEngine.UI;
 
 public class FirstPersonLook : MonoBehaviour
 {
@@ -11,6 +15,36 @@ public class FirstPersonLook : MonoBehaviour
     Vector2 frameVelocity;
 
 
+    DatabaseReference reference;
+    [SerializeField] float velocityY;
+    [SerializeField] float vecRightX;
+    [SerializeField] float vecRightY;
+    [SerializeField] float vecRightZ;
+    [SerializeField] float velocityX;
+    [SerializeField] float vecUpX;
+    [SerializeField] float vecUpY;
+    [SerializeField] float vecUpZ;
+
+     float velocityYkopie;
+     float vecRightXkopie;
+     float vecRightYkopie;
+     float vecRightZkopie;
+     float velocityXkopie;
+     float vecUpXkopie;
+     float vecUpYkopie;
+     float vecUpZkopie;
+
+    private string speicherVelY;
+    private string speicherRightX;
+    private string speicherRightY;
+    private string speicherRightZ;
+    private string speicherVelX;
+    private string speicherUpX;
+    private string speicherUpY;
+    private string speicherUpZ;
+
+    public KeyCode space = KeyCode.Space;
+
     void Reset()
     {
         // Get the character from the FirstPersonMovement in parents.
@@ -21,19 +55,213 @@ public class FirstPersonLook : MonoBehaviour
     {
         // Lock the mouse cursor to the game screen.
         Cursor.lockState = CursorLockMode.Locked;
+        reference = FirebaseDatabase.DefaultInstance.RootReference;  
+
     }
 
     void Update()
     {
-        // Get smooth velocity.
-        Vector2 mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
-        Vector2 rawFrameVelocity = Vector2.Scale(mouseDelta, Vector2.one * sensitivity);
-        frameVelocity = Vector2.Lerp(frameVelocity, rawFrameVelocity, 1 / smoothing);
-        velocity += frameVelocity;
-        velocity.y = Mathf.Clamp(velocity.y, -90, 90);
+        
 
-        // Rotate camera up-down and controller left-right from velocity.
-        transform.localRotation = Quaternion.AngleAxis(-velocity.y, Vector3.right);
-        character.localRotation = Quaternion.AngleAxis(velocity.x, Vector3.up);
+
+        if(Input.GetKey(space))
+        {
+
+
+            reference.Child("Body").Child("Look").Child("velocityY").GetValueAsync().ContinueWith(task =>
+            {
+                if(task.IsCompleted)
+                {
+                    //Debug.Log("retrived");
+                    DataSnapshot snapshot = task.Result;
+                    //Debug.Log(snapshot.Value.ToString());
+                    speicherVelY = snapshot.Value.ToString();
+                    
+                }
+                else
+                {
+                    Debug.Log("not retrived");
+                }
+                
+            }); 
+            //velocityYkopie = float.Parse(speicherVelY);
+            velocityYkopie = float.Parse(speicherVelY);
+
+            reference.Child("Body").Child("Look").Child("velocityX").GetValueAsync().ContinueWith(task =>
+            {
+                if(task.IsCompleted)
+                {
+                    //Debug.Log("retrived");
+                    DataSnapshot snapshot = task.Result;
+                    //Debug.Log(snapshot.Value.ToString());
+                    speicherVelX = snapshot.Value.ToString();
+                    
+                }
+                else
+                {
+                    Debug.Log("not retrived");
+                }
+                
+            }); 
+            velocityXkopie = float.Parse(speicherVelX);
+
+
+            reference.Child("Body").Child("Look").Child("vecRightX").GetValueAsync().ContinueWith(task =>
+            {
+                if(task.IsCompleted)
+                {
+                    //Debug.Log("retrived");
+                    DataSnapshot snapshot = task.Result;
+                    //Debug.Log(snapshot.Value.ToString());
+                    speicherRightX = snapshot.Value.ToString();
+                    
+                }
+                else
+                {
+                    Debug.Log("not retrived");
+                }
+                
+            }); 
+            vecRightXkopie = float.Parse(speicherRightX);
+
+            reference.Child("Body").Child("Look").Child("vecRightY").GetValueAsync().ContinueWith(task =>
+            {
+                if(task.IsCompleted)
+                {
+                    //Debug.Log("retrived");
+                    DataSnapshot snapshot = task.Result;
+                    //Debug.Log(snapshot.Value.ToString());
+                    speicherRightY = snapshot.Value.ToString();
+                    
+                }
+                else
+                {
+                    Debug.Log("not retrived");
+                }
+                
+            }); 
+            vecRightYkopie = float.Parse(speicherRightY);
+
+            reference.Child("Body").Child("Look").Child("vecRightZ").GetValueAsync().ContinueWith(task =>
+            {
+                if(task.IsCompleted)
+                {
+                    //Debug.Log("retrived");
+                    DataSnapshot snapshot = task.Result;
+                    //Debug.Log(snapshot.Value.ToString());
+                    speicherRightZ = snapshot.Value.ToString();
+                    
+                }
+                else
+                {
+                    Debug.Log("not retrived");
+                }
+                
+            }); 
+            vecRightZkopie = float.Parse(speicherRightZ);
+
+            reference.Child("Body").Child("Look").Child("vecUpX").GetValueAsync().ContinueWith(task =>
+            {
+                if(task.IsCompleted)
+                {
+                    //Debug.Log("retrived");
+                    DataSnapshot snapshot = task.Result;
+                    //Debug.Log(snapshot.Value.ToString());
+                    speicherUpX = snapshot.Value.ToString();
+                    
+                }
+                else
+                {
+                    Debug.Log("not retrived");
+                }
+                
+            }); 
+            vecUpXkopie = float.Parse(speicherUpX);
+
+            reference.Child("Body").Child("Look").Child("vecUpY").GetValueAsync().ContinueWith(task =>
+            {
+                if(task.IsCompleted)
+                {
+                    //Debug.Log("retrived");
+                    DataSnapshot snapshot = task.Result;
+                    //Debug.Log(snapshot.Value.ToString());
+                    speicherUpY = snapshot.Value.ToString();
+                    
+                }
+                else
+                {
+                    Debug.Log("not retrived");
+                }
+                
+            }); 
+            vecUpYkopie = float.Parse(speicherUpY);
+
+            reference.Child("Body").Child("Look").Child("vecUpZ").GetValueAsync().ContinueWith(task =>
+            {
+                if(task.IsCompleted)
+                {
+                    //Debug.Log("retrived");
+                    DataSnapshot snapshot = task.Result;
+                    //Debug.Log(snapshot.Value.ToString());
+                    speicherUpZ = snapshot.Value.ToString();
+                    
+                }
+                else
+                {
+                    Debug.Log("not retrived");
+                }
+                
+            }); 
+            vecUpZkopie = float.Parse(speicherUpZ);
+
+
+            //Debug.Log("veloY:" + -velocityYkopie);
+            //Debug.Log("veloX:" + velocityXkopie);
+
+        
+
+
+            // Get smooth velocity.
+            // Rotate camera up-down and controller left-right from velocity.
+            transform.localRotation = Quaternion.AngleAxis(-velocityYkopie, new Vector3(vecRightXkopie, vecRightYkopie, vecRightZkopie));
+            character.localRotation = Quaternion.AngleAxis(velocityXkopie, new Vector3(vecUpXkopie, vecUpYkopie, vecUpZkopie));
+
+
+        }
+        else{
+
+            // Get smooth velocity.
+            Vector2 mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+            Vector2 rawFrameVelocity = Vector2.Scale(mouseDelta, Vector2.one * sensitivity);
+            frameVelocity = Vector2.Lerp(frameVelocity, rawFrameVelocity, 1 / smoothing);
+            velocity += frameVelocity;
+            velocity.y = Mathf.Clamp(velocity.y, -90, 90);
+
+            // Rotate camera up-down and controller left-right from velocity.
+            transform.localRotation = Quaternion.AngleAxis(-velocity.y, Vector3.right);
+            character.localRotation = Quaternion.AngleAxis(velocity.x, Vector3.up);
+
+            velocityY = -velocity.y;
+            vecRightX = Vector3.right.x;
+            vecRightY = Vector3.right.y;
+            vecRightZ = Vector3.right.z;
+            velocityX = velocity.x;
+            vecUpX = Vector3.up.x;
+            vecUpY = Vector3.up.y;
+            vecUpZ = Vector3.up.z;
+
+            reference.Child("Body").Child("Look").Child("velocityY").SetValueAsync(velocityY.ToString());
+            reference.Child("Body").Child("Look").Child("vecRightX").SetValueAsync(vecRightX.ToString());
+            reference.Child("Body").Child("Look").Child("vecRightY").SetValueAsync(vecRightY.ToString());
+            reference.Child("Body").Child("Look").Child("vecRightZ").SetValueAsync(vecRightZ.ToString());
+            reference.Child("Body").Child("Look").Child("velocityX").SetValueAsync(velocityX.ToString());
+            reference.Child("Body").Child("Look").Child("vecUpX").SetValueAsync(vecUpX.ToString());
+            reference.Child("Body").Child("Look").Child("vecUpY").SetValueAsync(vecUpY.ToString());
+            reference.Child("Body").Child("Look").Child("vecUpZ").SetValueAsync(vecUpZ.ToString());
+
+        }
+
+        
+        
     }
 }
--- IP6_BasicShowroom 2/Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
+++ IP6_BasicShowroom 2/Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
@@ -1,21 +1,36 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using Firebase.Database;
+using UnityEngine.UI;
 
 public class FirstPersonMovement : MonoBehaviour
 {
     public float speed = 5;
+
+    DatabaseReference reference;
+    [SerializeField] float rigidbodyX;
+    [SerializeField] float rigidbodyY;
+    [SerializeField] float rigidbodyZ;
+
+    float rigidbodyXkopie;
+    float rigidbodyYkopie;
+    float rigidbodyZkopie;
+
+    private string speicherX;
+    private string speicherY;
+    private string speicherZ;
 
     [Header("Running")]
     public bool canRun = true;
     public bool IsRunning { get; private set; }
     public float runSpeed = 9;
     public KeyCode runningKey = KeyCode.LeftShift;
+    public KeyCode space = KeyCode.Space;
 
     Rigidbody rigidbody;
     /// <summary> Functions to override movement speed. Will use the last added override. </summary>
     public List<System.Func<float>> speedOverrides = new List<System.Func<float>>();
-
-
 
     void Awake()
     {
@@ -23,10 +38,17 @@ public class FirstPersonMovement : MonoBehaviour
         rigidbody = GetComponent<Rigidbody>();
     }
 
+     void Start()
+    {
+        reference = FirebaseDatabase.DefaultInstance.RootReference;  
+    }
+
     void FixedUpdate()
     {
         // Update IsRunning from input.
         IsRunning = canRun && Input.GetKey(runningKey);
+
+        
 
         // Get targetMovingSpeed.
         float targetMovingSpeed = IsRunning ? runSpeed : speed;
@@ -35,10 +57,94 @@ public class FirstPersonMovement : MonoBehaviour
             targetMovingSpeed = speedOverrides[speedOverrides.Count - 1]();
         }
 
-        // Get targetVelocity from input.
-        Vector2 targetVelocity =new Vector2( Input.GetAxis("Horizontal") * targetMovingSpeed, Input.GetAxis("Vertical") * targetMovingSpeed);
+        if(Input.GetKey(space))
 
-        // Apply movement.
-        rigidbody.velocity = transform.rotation * new Vector3(targetVelocity.x, rigidbody.velocity.y, targetVelocity.y);
+        {
+
+            reference.Child("Body").Child("Position").Child("rigidbodyX").GetValueAsync().ContinueWith(task =>
+            {
+            if(task.IsCompleted)
+            {
+                //Debug.Log("retrived");
+                DataSnapshot snapshot = task.Result;
+                //Debug.Log(snapshot.Value.ToString());
+                speicherX = snapshot.Value.ToString();
+                
+            }
+            else
+            {
+                Debug.Log("not retrived");
+            }
+            
+            }); 
+            rigidbodyXkopie = float.Parse(speicherX);
+
+            reference.Child("Body").Child("Position").Child("rigidbodyY").GetValueAsync().ContinueWith(task =>
+            {
+                if(task.IsCompleted)
+                {
+                    //Debug.Log("retrived");
+                    DataSnapshot snapshot = task.Result;
+                    //Debug.Log(snapshot.Value.ToString());
+                    speicherY = snapshot.Value.ToString();
+                    
+                }
+                else
+                {
+                    Debug.Log("not retrived");
+                }
+                
+            }); 
+            rigidbodyYkopie = float.Parse(speicherY);
+
+            reference.Child("Body").Child("Position").Child("rigidbodyZ").GetValueAsync().ContinueWith(task =>
+            {
+                if(task.IsCompleted)
+                {
+                    //Debug.Log("retrived");
+                    DataSnapshot snapshot = task.Result;
+                    //Debug.Log(snapshot.Value.ToString());
+                    speicherZ = snapshot.Value.ToString();
+                }
+                else
+                {
+                    Debug.Log("not retrived");
+                }
+                
+            }); 
+            rigidbodyZkopie = float.Parse(speicherZ);
+            
+            // Apply movement.
+        //rigidbody.velocity = transform.rotation * new Vector3(targetVelocity.x, rigidbody.velocity.y, targetVelocity.y);
+            rigidbody.velocity = transform.rotation *  new Vector3(rigidbodyXkopie, rigidbodyYkopie, rigidbodyZkopie);
+
+            Debug.Log("Kopie X" + rigidbodyXkopie);
+            Debug.Log("Kopie Y" + rigidbodyXkopie);
+            Debug.Log("Kopie Z" + rigidbodyXkopie);
+
+        }
+
+        else
+
+        {
+            // Get targetVelocity from input.
+            Vector2 targetVelocity = new Vector2( Input.GetAxis("Horizontal") * targetMovingSpeed, Input.GetAxis("Vertical") * targetMovingSpeed);
+
+            rigidbodyX = targetVelocity.x;
+            rigidbodyY = rigidbody.velocity.y;
+            rigidbodyZ = targetVelocity.y;
+
+            reference.Child("Body").Child("Position").Child("rigidbodyX").SetValueAsync(rigidbodyX.ToString());
+            Debug.Log("string X: " + rigidbodyX.ToString());
+            reference.Child("Body").Child("Position").Child("rigidbodyY").SetValueAsync(rigidbodyY.ToString());
+            Debug.Log("string Y: " + rigidbodyY.ToString());
+            reference.Child("Body").Child("Position").Child("rigidbodyZ").SetValueAsync(rigidbodyZ.ToString());
+            Debug.Log("string Z: " + rigidbodyZ.ToString());
+
+            // Apply movement.
+            rigidbody.velocity = transform.rotation * new Vector3(targetVelocity.x, rigidbody.velocity.y, targetVelocity.y);
+            //rigidbody.velocity = transform.rotation *  new Vector3(rigidbodyX, rigidbodyY, rigidbodyZ);
+        }
+        
     }
 }
