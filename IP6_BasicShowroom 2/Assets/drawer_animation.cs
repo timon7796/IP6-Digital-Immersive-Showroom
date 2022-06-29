1d910479e68589c7b58b04387d54dd85ad9cc874
TimonKeller <55238597+TimonKeller@users.noreply.github.com>
2022-06-29 16:47:23 +0200

    Update drawer_animation.cs

--- IP6_BasicShowroom 2/Assets/drawer_animation.cs
+++ IP6_BasicShowroom 2/Assets/drawer_animation.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class drawer_animation : MonoBehaviour
 {
-      public float movementSpeed = 0.001f;
+    public float movementSpeed = 0.001f;
+    private float count= 0f;
  
     private Rigidbody rb;
     private Vector3 endPosition = new Vector3(382.7423f, 1.485f, 109.136f);
+    private Vector3 startPosition = new Vector3(381.877f, 1.485f, 109.512f);
     // Use this for initialization
     void Start() {
         rb = GetComponent<Rigidbody>();
@@ -15,13 +17,20 @@ public class drawer_animation : MonoBehaviour
  
     // Update is called once per frame
     void Update() {
-        if(Input.GetKeyDown(KeyCode.Alpha1)){
-            Debug.Log("Alpha 1");
+        if((count % 2f == 0) && Input.GetKeyDown(KeyCode.Alpha1)){
             if(rb.position != endPosition) {
                 Vector3 newPosition = Vector3.MoveTowards(rb.position, endPosition, movementSpeed);
                 rb.MovePosition(newPosition);
+                count++;
             }
         }
-       
-    }
+        else if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if(rb.position != startPosition) {
+                Vector3 newPosition = Vector3.MoveTowards(rb.position, startPosition, movementSpeed);
+                rb.MovePosition(newPosition);
+                count++;
+            }
+        }   
+    }   
 }
